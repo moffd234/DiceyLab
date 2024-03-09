caf08da0a856d9d75da8fdf67d29876d7785a72f
moffd234 <moffd234@gmail.com>
2024-03-08 21:30:46 -0500

    Setup BinsTest and Bins classes

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bins {
+
+    Map<Integer, Integer> rolls = new HashMap<>();
+    int minRoll, maxRoll;
+
+    Bins(int minRoll, int maxRoll){
+        this.minRoll = minRoll;
+        this.maxRoll = maxRoll;
+        setupMap();
+    }
+
+    public void setupMap(){
+        for(int i = minRoll; i <= maxRoll; i++){
+            rolls.put(i, 0);
+        }
+    }
+
+    public int getBin(int num){
+        return rolls.get(num);
+    }
+
+    public void incrementBin(int num){
+        int oldVal = getBin(num);
+        rolls.replace(num, oldVal + 1);
+    }
+    
+    
 
 }
---
+++ src/test/java/BinsTest.java
@@ -0,0 +1,107 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinsTest {
+    @Test
+    public void testSetupMap(){
+        int minNum = 2;
+        int maxNum = 12;
+        Bins bins = new Bins(minNum, maxNum);
+
+        for(int i = minNum; i <= maxNum; i++){
+            Assert.assertTrue(bins.rolls.containsKey(i));
+            Assert.assertEquals(0, (int) bins.rolls.get(i));
+        }
+    }
+
+    @Test
+    public void testSetupMap1(){
+        int minNum = 5;
+        int maxNum = 30;
+        Bins bins = new Bins(minNum, maxNum);
+
+        for(int i = minNum; i <= maxNum; i++){
+            Assert.assertTrue(bins.rolls.containsKey(i));
+            Assert.assertEquals(0, (int) bins.rolls.get(i));
+        }
+    }
+
+    @Test
+    public void testGetBin(){
+        int minNum = 2;
+        int maxNum = 12;
+        Bins bins = new Bins(minNum, maxNum);
+
+        int expected = 0;
+        int actual = bins.getBin(2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetBin1(){
+        int minNum = 5;
+        int maxNum = 30;
+        Bins bins = new Bins(minNum, maxNum);
+
+        int expected = 0;
+        int actual = bins.getBin(2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetBinAfterIncrement(){
+        int minNum = 2;
+        int maxNum = 12;
+        Bins bins = new Bins(minNum, maxNum);
+
+        int expected = 1;
+        bins.incrementBin(minNum);
+        int actual = bins.getBin(minNum);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetBinAfterIncrement1(){
+        int minNum = 5;
+        int maxNum = 30;
+
+        int expected = 1;
+        Bins bins = new Bins(minNum, maxNum);
+
+        bins.incrementBin(minNum);
+        int actual = bins.getBin(minNum);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncrementBin(){
+
+        int minNum = 2;
+        int maxNum = 12;
+        Bins bins = new Bins(minNum, maxNum);
+        int expected = 1;
+
+        bins.incrementBin(minNum);
+        int actual = bins.getBin(minNum);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncrementBin1(){
+
+        int minNum = 5;
+        int maxNum = 30;
+        Bins bins = new Bins(minNum, maxNum);
+        int expected = 1;
+
+        bins.incrementBin(minNum);
+        int actual = bins.getBin(minNum);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
