ba03e24f0bee45dacf8ec9ec96ed31a3a8d2696d
moffd234 <moffd234@gmail.com>
2024-03-08 20:51:33 -0500

    Created Simulation constructor

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,6 +1,8 @@
 public class Simulation {
-    Simulation(){
-
+    int numberOfDies, numberOfTosses;
+    Simulation(int numberOfDies, int numberOfTosses){
+        this.numberOfDies = numberOfDies;
+        this.numberOfTosses = numberOfTosses;
     }
     public static void main(String[] args){}
 
